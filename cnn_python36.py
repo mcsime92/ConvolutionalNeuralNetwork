cf40b48324fd01081e33f0d6736dbb7ac24671d8
mcsime92 <godfroidmaxime@hotmail.com>
2019-04-23 15:24:44 +0200

    Fit classifier

--- cnn_python36.py
+++ cnn_python36.py
@@ -28,3 +28,30 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))
 # Binary outcome, hence binary loss function
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+### Fit the CNN to images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale= 1./255)
+
+training_set = train_datagen.flow_from_directory(r'C:\Users\A747043\Desktop\My documents\Python\Python36\PyCharm '
+                                                 r'Projects\Deep Learning\Deep_Learning_A_Z\Volume 1 - Supervised '
+                                                 r'Deep Learning\Part 2 - CNN\Section 8 - Building CNN\dataset\training_set',
+                                                 target_size=(64,64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(r'C:\Users\A747043\Desktop\My documents\Python\Python36\PyCharm '
+                                            r'Projects\Deep Learning\Deep_Learning_A_Z\Volume 1 - Supervised '
+                                            r'Deep Learning\Part 2 - CNN\Section 8 - Building CNN\dataset\test_set',
+                                            target_size=(64,64),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+classifier.fit_generator(training_set, steps_per_epoch=8000, epochs = 10,
+                         validation_data=test_set, validation_steps= 2000)
+
